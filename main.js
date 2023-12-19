2d73ba1a9e85a84f4c0c2dbab01d24c810cc426d
mattbreda <mattbreda@gmail.com>
2023-12-19 12:52:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ import { loaderModal } from "./scripts/domElements";
 // settiamo un listener "DOMContentLoaded", ovvero, una volta che il browser avrà renderizzato il dom
 // (quindi solo l'html e il css), andiamo a mettere un timer di 3000ms (3 secondi), che allo scadere,
 // carichi tutti i pokemon, i tipi e nasconda il loader
+const pippo
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(() => {
     displayPokemons();
